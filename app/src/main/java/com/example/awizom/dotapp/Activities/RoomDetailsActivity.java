4fd875199d40c6a75b98c2d73013cf0c2df201b4
user <siddharthnayak0223@gmail.com>
2018-09-21 19:08:08 +0530

    layout create, Activities folder with activity create,include String file,dimen file, Style

---
+++ app/src/main/java/com/example/awizom/dotapp/Activities/RoomDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.awizom.dotapp.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.awizom.dotapp.R;
+
+public class RoomDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.room_details);
+
+    }
+}
